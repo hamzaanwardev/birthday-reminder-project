5c95d5fa831a0fb881182d50c1ff99499e83b439
Hamza Anwar <88555920+hamzaanwardev@users.noreply.github.com>
2022-09-15 08:27:11 +0500

    making the list of birthday people

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -5,8 +5,8 @@ function App() {
   const [people, setPeople] = useState(data);
   return <main>
     <section className='container'>
-      <h3>0 birthdays today</h3>
-      <List/>
+      <h3>{people.length} birthdays today</h3>
+      <List people={people}/>
       <button onClick={() => console.log('You clicked me')}> Clear All</button>
     </section>
   </main>;
